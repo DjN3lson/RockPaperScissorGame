d9a9a6af212644329eefa48d16f4584fa479c557
Pablo Carmona <carmonpa@sheridancollege.ca>
2023-11-13 01:56:10 -0500

    app.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {HeaderComponent} from "./header/header.component";
-import {FooterComponent} from "./footer/footer.component";
 import {InputComponent} from "./input-header/input.component";
 import {ResultComponent} from "./result-page/result.component";
-import{AppComponent} from "./app.component";
-import {FormsModule} from "@angular/forms";
 
 const routes: Routes = [
   { path: '', redirectTo: '/input', pathMatch: 'full' },
@@ -14,14 +10,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    ResultComponent,
-    FooterComponent,
-    InputComponent,
-    AppComponent
-  ],
-  imports: [RouterModule.forRoot(routes), FormsModule],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+
+  exports: [RouterModule],
+
+  declarations: [  ]
+
 })
 export class AppRoutingModule { }
